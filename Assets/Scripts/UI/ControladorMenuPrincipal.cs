a5435c22fcfc4ad5319da909d78166e9133b7b74
Arthur Silva Aguiar <arthur94_br@hotmail.com>
2016-10-13 18:06:41 -0300

    UI - MenuPrincipal
    Adicionei os Assets dos UI do Menu Principal.
    E Montei a cena do Menu Principal

---
+++ Assets/Scripts/UI/ControladorMenuPrincipal.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+//Classe responsável por gerenciar todo o Menu Principal
+public class ControladorMenuPrincipal : MonoBehaviour {
+    // Botão Start // Ao clicar no botão ele carrega a cena "LevelSelect"
+    private string level = "LevelSelect";
+    public void Botao_start()
+    {
+        SceneManager.LoadScene(level);
+    }
+    // Botão de configurações // Ao clicar ele se expande(Dropdown) e habilita os botões de SOM,SFX e Idioma.
+    public void Botao_Config()
+    {
+        GameObject.Find("Config").GetComponent<DropDownMenu1>().Verifica();
+    }
+    // Botão de configurações // Ao clicar ele se expande(Dropdown) e habilita os botões de Conquistas e Ranking.
+    public void Botao_Online()
+    {
+        GameObject.Find("Online Service").GetComponent<DropDownMenu2>().Verifica();
+    }
+}
---
+++ Assets/Scripts/UI/DropDownMenu2.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+//Classe responsável por expandir o Botão de Serviços Online Habilitando Conquistas e Ranking.
+public class DropDownMenu2 : MonoBehaviour {
+
+    public RectTransform container;
+    private bool IsOpen;
+    void Start()
+    {
+
+        container = transform.FindChild("Container2").GetComponent<RectTransform>();
+        IsOpen = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 scale = container.localScale;
+        scale.x = Mathf.Lerp(scale.x, (IsOpen == true) ? 1 : 0, Time.deltaTime * 10);
+        container.localScale = scale;
+    }
+    //Metodo que verifica se o Botão(Serviços Online) está aberto quando clicado, se sim ele o fecha, senão ele o abre.
+    public void Verifica()
+    {
+        if (IsOpen == true)
+        {
+            IsOpen = false;
+        }
+        else IsOpen = true;
+    }
+}
