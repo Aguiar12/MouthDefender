190b847a2297340c18a4f4e0e3da55b6bd115078
Rafael Santos <rafaelnsantos@users.noreply.github.com>
2016-10-18 18:27:58 -0200

    add bocejo e animações do dente de cima randomicas

--- Assets/Scripts/DenteCima.cs
+++ Assets/Scripts/DenteCima.cs
@@ -2,38 +2,34 @@
 using System.Collections;
 
 public class DenteCima : MonoBehaviour {
-	public int tempo = 100;
-	public float velocidade = 0.005f;
-	public int pausa = 2;
-	private int aux = 0;
+	private int cont;
+	private bool mastigando;
+	private bool bocejando;
 
 	// Use this for initialization
 	void Start () {
+		mastigando = false;
+		bocejando = false;
+		cont = 0;
 	}
 	// Update is called once per frame
 	void Update () {
-		if(aux < 1 * tempo) {
-			baixo(velocidade);
-			direita(velocidade/3);
-			aux++;
-		} else if (aux < 2 * tempo) {
-			baixo(velocidade);
-			esquerda(velocidade/3);
-			aux++;
-		} else if (aux < 3 * tempo) {
-			cima(velocidade);
-			esquerda(velocidade/3);
-			aux++;
-		} else if (aux < 4 * tempo) {
-			cima(velocidade);
-			direita(velocidade/3);
-			aux++;
-		} else if (aux < (4 + pausa) * tempo){
-			aux++;
-		} else {
-			aux = 0;
+		if(!mastigando && !bocejando){
+			int random = Random.Range(0, 200);
+			if(random == 0){
+				mastigando = true;
+			} else if(random == 1){
+				bocejando = true;
+			}
 		}
-		
+
+		if(mastigando){
+			mastigar(40, 0.02f);
+		}
+
+		if(bocejando){
+			bocejar(80, 0.01f);
+		}
 	}
 	public void cima(float y){
 		transform.Translate(new Vector3(0, y));
@@ -46,5 +42,38 @@ public class DenteCima : MonoBehaviour {
 	}
 	public void esquerda(float x){
 		transform.Translate(new Vector3(-x, 0));
+	}
+	private void mastigar(int tempo, float velocidade) {
+		if(cont < 1 * tempo) {
+			baixo(velocidade);
+			direita(velocidade/3);
+		} else if (cont < 2 * tempo) {
+			baixo(velocidade);
+			esquerda(velocidade/3);
+		} else if (cont < 3 * tempo) {
+			cima(velocidade);
+			esquerda(velocidade/3);
+		} else if (cont < 4 * tempo) {
+			cima(velocidade);
+			direita(velocidade/3);
+		} else {
+			cont = 0;
+			mastigando = false;
+		}
+		cont++;
+	}
+
+	private void bocejar(int tempo, float velocidade) {
+		if(cont < 2 * tempo) {
+			cima(velocidade);
+		} else if(cont < 4 * tempo) {
+			
+		} else if(cont < 6 * tempo) {
+			baixo(velocidade);
+		} else {
+			cont = 0;
+			bocejando = false;
+		}
+		cont++;
 	}
 }
